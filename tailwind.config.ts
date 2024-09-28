c5df221ca145acde34da2a9e1335e3bdad68323c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-28 20:33:19 +0630

    add font family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,10 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        poppin: "Poppins",
+        lora: "Lora",
+      },
     },
   },
   plugins: [],
