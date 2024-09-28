b2ca1ecae9fb598c31664364448585e6fdba36ce
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-28 20:42:42 +0630

    clean up file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section>
-      <h1>hello</h1>
-      <h2 className="font-poppin">poppin</h2>
-      <h3 className="font-lora">
-        lora Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia quod
-        error nemo dolores nulla fuga ab, numquam mollitia aspernatur provident
-        eligendi unde, ut, corrupti enim aliquam. Molestias facere eos corporis!
-      </h3>
+      <h1 className="font-serif">hi</h1>
     </section>
   );
 }
