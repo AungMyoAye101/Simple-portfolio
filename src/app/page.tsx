86e3910b96ce54d92bad8837e4932667aff12c82
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-29 13:04:25 +0630

    add recent project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
+import Project from "@/components/Project";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <section>
       <Hero />
+      <Project />
     </section>
   );
 }
---
+++ src/components/Project.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Project = () => {
+  return (
+    <section>
+      <h1 className="font-lora text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center py-20">
+        My Recent Projects
+      </h1>
+    </section>
+  );
+};
+
+export default Project;
