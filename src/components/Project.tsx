f8ac72fe818739d2aa49e1c5f8f9dabb9de75fcf
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-29 13:35:02 +0630

    update card

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -8,7 +8,7 @@ const Project = () => {
       </h1>
       <main>
         <div className="border border-gray-300 rounded-md shadow-md w-96">
-          <div className="w-full bg-gray-300 h-40"></div>
+          <div className="w-full bg-gray-300 h-48"></div>
           <div className="p-4 space-y-2">
             <h1 className="font-serif text-xl md:text-2xl  font-semibold">
               My Project Name
@@ -17,16 +17,23 @@ const Project = () => {
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
               quidem vitae, quas libero dolor ea laudantium? Eius voluptatum
             </p>
+            <div className="flex items-center ">
+              <div className="size-10 border-[0.5px] border-gray-300 rounded-full  "></div>
+              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
+              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
+              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
+              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
+            </div>
             <div className="flex justify-between items-center text-xs md:text-sm font-poppin py-2">
               <a
                 href="#"
-                className="px-2 py-1 border border-gray-300 rounded shadow-md"
+                className="px-3 py-2 border border-gray-300 rounded shadow-md"
               >
                 View Source Code
               </a>
               <a
                 href="#"
-                className="px-2 py-1 border border-gray-300 rounded shadow-md"
+                className="px-3 py-2 border border-gray-300 rounded shadow-md"
               >
                 Check Live Site
               </a>
