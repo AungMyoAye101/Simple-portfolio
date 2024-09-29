a8faa63f3212fd19f61df8d0ce9201838805a199
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-29 13:55:11 +0630

    update data

---
+++ src/data/index.ts
@@ -0,0 +1,57 @@
+export const projects = [
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+
+    iconList: ["1", "2", "3", "4", "5", "4", "5"],
+  },
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+    iconList: ["1", "2", "3", "4", "5"],
+  },
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+    iconList: ["1", "2", "3", "4", "5"],
+  },
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+    iconList: ["1", "2", "3", "4", "5"],
+  },
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+    iconList: ["1", "2", "3", "4", "5"],
+  },
+  {
+    image: "example",
+    title: "My Project Name",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit.  quidem vitae, quas libero dolor ea laudantium? ",
+    sourceCode: "example.com",
+    siteLink: "example.com",
+    iconList: ["1", "2", "3", "4", "5"],
+  },
+];
