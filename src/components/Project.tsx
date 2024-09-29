d42c5091989fab05fd96147eebf05bf613e4852e
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-29 13:41:30 +0630

    update card

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { projects } from "@/data";
 
 const Project = () => {
   return (
@@ -6,40 +7,43 @@ const Project = () => {
       <h1 className="font-lora text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-center ">
         My Recent Projects
       </h1>
-      <main>
-        <div className="border border-gray-300 rounded-md shadow-md w-96">
-          <div className="w-full bg-gray-300 h-48"></div>
-          <div className="p-4 space-y-2">
-            <h1 className="font-serif text-xl md:text-2xl  font-semibold">
-              My Project Name
-            </h1>
-            <p className="text-sm md:text-base font-light font-lora line-clamp-2">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est
-              quidem vitae, quas libero dolor ea laudantium? Eius voluptatum
-            </p>
-            <div className="flex items-center ">
-              <div className="size-10 border-[0.5px] border-gray-300 rounded-full  "></div>
-              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
-              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
-              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
-              <div className="size-10 border-[0.5px] border-gray-300 rounded-full -ml-2"></div>
-            </div>
-            <div className="flex justify-between items-center text-xs md:text-sm font-poppin py-2">
-              <a
-                href="#"
-                className="px-3 py-2 border border-gray-300 rounded shadow-md"
-              >
-                View Source Code
-              </a>
-              <a
-                href="#"
-                className="px-3 py-2 border border-gray-300 rounded shadow-md"
-              >
-                Check Live Site
-              </a>
+      <main className="grid sm:grid-cols-2 lg:grid-cols-3 place-items-center gap-8">
+        {projects.map((project, i) => (
+          <div key={i} className="border border-gray-300 rounded-md shadow-md ">
+            <div className="w-full bg-gray-300 h-48"></div>
+            <div className="p-4 space-y-2">
+              <h1 className="font-serif text-xl md:text-2xl  font-semibold">
+                {project.title}
+              </h1>
+              <p className="text-sm md:text-base font-light font-lora line-clamp-2">
+                {project.description}
+              </p>
+              <div className="flex items-center ">
+                {project.iconList.map((icon, index) => (
+                  <div
+                    className={`size-10 border-[0.5px] border-gray-300 rounded-full ${
+                      index > 0 && "-ml-2"
+                    }`}
+                  ></div>
+                ))}
+              </div>
+              <div className="flex justify-between items-center text-xs md:text-sm font-poppin py-2">
+                <a
+                  href="#"
+                  className="px-3 py-2 border border-gray-300 rounded shadow-md"
+                >
+                  View Source Code
+                </a>
+                <a
+                  href="#"
+                  className="px-3 py-2 border border-gray-300 rounded shadow-md"
+                >
+                  Check Live Site
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </main>
     </section>
   );
