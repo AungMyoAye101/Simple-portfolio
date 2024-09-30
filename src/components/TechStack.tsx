0a87a137ef140e6a8964acbaefd5ff56f44cd570
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-09-30 20:30:49 +0630

    update color of balls

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const TechStack = () => {
   return (
-    <section className="py-16 px-10 flex flex-col items-center gap-12 min-h-screen">
+    <section className="py-16 px-10 flex flex-col items-center gap-12 min-h-screen relative overflow-hidden">
       <div className="flex flex-col items-center gap-6">
         <h1 className="font-lora text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
           My Tech Stack
@@ -52,18 +52,18 @@ const TechStack = () => {
           <p className="font-lora font-semibold text-sm">React Js</p>
         </div>
       </main> */}
-      <div>
+      <div className="absolute -right-40 top-[30%] -z-10">
         <div className="p-20 rounded-full border border-gray-400 relative animate-spin-slow">
-          <div className="size-14 rounded-full z-20 bg-purple-400 absolute -bottom-6 left-[50%]"></div>
-          <div className="size-14 rounded-full z-20 bg-red-400 absolute -top-6 right-[50%]"></div>
-          <div className="size-14 rounded-full z-20 bg-green-400 absolute bottom-[50%] -left-6"></div>
-          <div className="size-14 rounded-full z-20 bg-sky-400 absolute bottom-[50%] -right-6"></div>
+          <div className="size-14 rounded-full z-20 bg-green-500 absolute -bottom-6 left-[50%]"></div>
+          <div className="size-14 rounded-full z-20 bg-red-500 absolute -top-6 right-[50%]"></div>
+          <div className="size-14 rounded-full z-20 bg-cyan-400 absolute bottom-[50%] -left-6"></div>
+          <div className="size-14 rounded-full z-20 bg-orange-400 absolute bottom-[50%] -right-6"></div>
           <div className="p-14  rounded-full relative border border-gray-400 animate-spin-slow">
-            <div className="size-14 rounded-full z-20 bg-purple-400 absolute -bottom-6 left-[50%]"></div>
-            <div className="size-14 rounded-full z-20 bg-red-400 absolute -top-6 right-[40%]"></div>
-            <div className="size-14 rounded-full z-20 bg-green-400 absolute bottom-[50%] -left-6"></div>
-            <div className="size-14 rounded-full z-20 bg-sky-400 absolute bottom-[40%] -right-6"></div>
-            <div className="size-40 bg-green-200 rounded-full"></div>
+            <div className="size-14 rounded-full z-20 bg-yellow-400 absolute -bottom-6 left-[50%]"></div>
+            <div className="size-14 rounded-full z-20 bg-violet-500 absolute -top-6 right-[40%]"></div>
+            <div className="size-14 rounded-full z-20 bg-rose-400 absolute bottom-[50%] -left-6"></div>
+            <div className="size-14 rounded-full z-20 bg-teal-500 absolute bottom-[40%] -right-6"></div>
+            <div className="size-40 bg-blue-500 rounded-full"></div>
           </div>
         </div>
       </div>
