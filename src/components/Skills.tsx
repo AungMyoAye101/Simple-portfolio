eac0723373ace7dbdb5f05c58f031be7ae665192
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-02 18:49:11 +0630

    add skills

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -11,10 +11,50 @@ const Skills = () => {
         <h1 className="font-lora text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold">
           My Tech Stack
         </h1>
-        <div className="font-poppin text-lg sm:text-xl md:text-2xl lg:text-3xl  flex items-center gap-10 ">
-          <span>Front-End Development </span>
-          <span>Back-End Development </span>
-          <span>Database & Others </span>
+      </div>
+      {/* My Skills  */}
+      <div className="flex gap-4">
+        <div className="max-w-80 px-4 py-6 rounded-xl shadow-md border-2 border-gray-300 ">
+          <Image src={"/react.svg"} width={40} height={40} alt="icons" />
+          <div className="space-y-2 mt-2">
+            <h1 className="font-poppin text-lg md:text-xl font-semibold">
+              Fornt-End Development
+            </h1>
+            <p className=" text-sm md:text-base text-balance">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              quaerat beatae minima architecto numquam, autem animi dolore,
+              aliquam expedita fugiat voluptatibus voluptate sequi voluptas!
+              Soluta, aperiam. Ab excepturi laudantium saepe!
+            </p>
+          </div>
+        </div>
+        <div className="max-w-80 p-4 rounded-xl shadow-md border-2 border-gray-300 ">
+          <Image src={"/nextjs.svg"} width={40} height={40} alt="icons" />
+          <div className="space-y-2 mt-2">
+            <h1 className="font-poppin text-lg md:text-xl font-semibold">
+              Back-End Development
+            </h1>
+            <p className=" text-sm md:text-base text-balance">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              quaerat beatae minima architecto numquam, autem animi dolore,
+              aliquam expedita fugiat voluptatibus voluptate sequi voluptas!
+              Soluta, aperiam. Ab excepturi laudantium saepe!
+            </p>
+          </div>
+        </div>
+        <div className="max-w-80 p-4 rounded-xl shadow-md border-2 border-gray-300 ">
+          <Image src={"/mongodb.svg"} width={40} height={40} alt="icons" />
+          <div className="space-y-2 mt-2">
+            <h1 className="font-poppin text-lg md:text-xl font-semibold">
+              Database & Others
+            </h1>
+            <p className=" text-sm md:text-base text-balance">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet
+              quaerat beatae minima architecto numquam, autem animi dolore,
+              aliquam expedita fugiat voluptatibus voluptate sequi voluptas!
+              Soluta, aperiam. Ab excepturi laudantium saepe!
+            </p>
+          </div>
         </div>
       </div>
 
