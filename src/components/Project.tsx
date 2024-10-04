1fda99c5fba2a8e88e508c08882a4712ffe0bb31
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-04 21:12:33 +0630

    update projects card

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -17,26 +17,28 @@ const Project = () => {
             <div className="relative w-full  md:w-[50%] bg-gray-600 h-44  md:h-64 rounded-lg overflow-hidden">
               <Image src={project.image} fill alt="project name" className="" />
             </div>
-            <div className="space-y-2 px-2  ">
-              <h1 className="text-xl md:text-2xl font-lora font-semibold">
-                My Project
-              </h1>
-              <p className="text-sm md:text-base font-poppin  font-medium line-clamp-2 md:line-clamp-none">
-                {project.description}
-              </p>
-              <div className="flex ">
-                {project.iconList.map((icon, idx) => (
-                  <div
-                    key={idx}
-                    className={`size-8 md;size-10 flex justify-center items-center rounded-full border border-gray-500 ${
-                      idx > 0 && "-ml-2"
-                    }`}
-                  >
-                    <Image src={icon} width={20} height={20} alt="icons" />
-                  </div>
-                ))}
+            <div className="flex flex-col justify-between px-2  ">
+              <div className="space-y-4">
+                <h1 className="text-xl md:text-2xl font-lora font-semibold">
+                  My Project
+                </h1>
+                <p className="text-sm md:text-base font-poppin  font-medium line-clamp-2 md:line-clamp-none">
+                  {project.description}
+                </p>
+                <div className="flex ">
+                  {project.iconList.map((icon, idx) => (
+                    <div
+                      key={idx}
+                      className={`size-8 md;size-10 flex justify-center items-center rounded-full border border-gray-500 ${
+                        idx > 0 && "-ml-2"
+                      }`}
+                    >
+                      <Image src={icon} width={20} height={20} alt="icons" />
+                    </div>
+                  ))}
+                </div>
               </div>
-              <div className="flex justify-between items-center pb-3">
+              <div className=" flex justify-between items-center pb-3">
                 <a
                   href="#"
                   className="text-lora font-medium text-sm md:text-base flex items-center gap-1 "
