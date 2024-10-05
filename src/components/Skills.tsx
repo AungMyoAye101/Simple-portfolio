d44a7462325574535a189a2b825e2661e6635217
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-05 13:42:44 +0630

    update skill set ui

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import TechSkills from "./TechSkills";
-import { backEnd, frontEnd, others } from "@/data";
+import { backEnd, frontEnd, others, skills } from "@/data";
 
 const Skills = () => {
   return (
@@ -11,49 +11,27 @@ const Skills = () => {
       </div>
       {/* My Skills  */}
       <div className="flex flex-wrap justify-center gap-6">
-        <div className="max-w-80 px-4 py-6 rounded-xl shadow-md border-2 border-gray-300 ">
-          <Image
-            src={"/react.svg"}
-            width={40}
-            height={40}
-            alt="icons"
-            className="mb-4"
-          />
+        {skills.map(({ id, image, title, description }) => (
+          <div
+            key={id}
+            className="max-w-80 px-4 py-6 rounded-xl shadow-md border-2 border-gray-300 "
+          >
+            <Image
+              src={image}
+              width={40}
+              height={40}
+              alt="icons"
+              className="mb-4"
+            />
 
-          <div className="space-y-3 mt-2">
-            <h1 className="font-poppin text-lg md:text-xl font-semibold">
-              UI & UX Design
-            </h1>
-            <p className=" para-heading">
-              UI/UX design expertise with a focus on user-friendly,
-              mobile-responsive interfaces using Figma.
-            </p>
+            <div className="space-y-3 mt-2">
+              <h1 className="font-poppin text-lg md:text-xl font-semibold">
+                {title}
+              </h1>
+              <p className=" para-heading">{description}</p>
+            </div>
           </div>
-        </div>
-        <div className="max-w-80 p-4 rounded-xl shadow-md border-2 border-gray-300 ">
-          <Image src={"/nextjs.svg"} width={40} height={40} alt="icons" />
-          <div className="space-y-2 mt-2">
-            <h1 className="font-poppin text-lg md:text-xl font-semibold">
-              Full-Stack Development
-            </h1>
-            <p className=" para-heading ">
-              Proficient in full-stack development using technologies such as
-              React, Next.js, Vue.js, Node.js, Express, and MongoDB.
-            </p>
-          </div>
-        </div>
-        <div className="max-w-80 p-4 rounded-xl shadow-md border-2 border-gray-300 ">
-          <Image src={"/mongodb.svg"} width={40} height={40} alt="icons" />
-          <div className="space-y-2 mt-2">
-            <h1 className="font-poppin text-lg md:text-xl font-semibold">
-              Database & Others
-            </h1>
-            <p className=" para-heading">
-              Experienced in designing and developing complex applications with
-              database management using Prisma, MySQL, and MongoDB.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
 
       <TechSkills title={frontEnd.title} skill={frontEnd.skills} />
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,3 +1,27 @@
+export const skills = [
+  {
+    id: 1,
+    image: "/react.svg",
+    title: " UI & UX Design",
+    description:
+      " UI/UX design expertise with a focus on user-friendly, mobile-responsive interfaces using Figma.",
+  },
+  {
+    id: 2,
+    image: "/nextjs.svg",
+    title: " Full-Stack Development",
+    description:
+      "Proficient in full-stack development using technologies such as  React, Next.js, Vue.js, Node.js, Express, and MongoDB.",
+  },
+  {
+    id: 3,
+    image: "/mongodb.svg",
+    title: " Database ",
+    description:
+      " Experienced in designing and developing complex applications with database management using Prisma, MySQL, and MongoDB.",
+  },
+];
+
 export const projects = [
   {
     image: "/p1.svg",
