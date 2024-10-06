ff707b4e412424743d83dec3b364533d12b67d96
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-06 13:49:54 +0630

    add animated nav bar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -6,60 +6,52 @@ import React from "react";
 
 const varients = {
   open: {
-    clipPath: "circle(1200px at 0 0)",
-    transition: {
-      duration: 1,
-    },
+    x: "0%",
   },
   close: {
-    clipPath: "circle(30px at -100px 0 )",
-    transition: {
-      duration: 1,
-    },
+    x: "-100%",
   },
 };
 
-const SideBar = ({ open }: { open: boolean }) => {
+const SideBar = ({ handleClick }: { handleClick: () => void }) => {
   return (
     <motion.div
-      animate={open ? "open" : "close"}
-      className={` flex justify-center items-center absolute transition-opacity duration-500 ease-out`}
+      variants={varients}
+      initial="close"
+      animate="open"
+      className="absolute top-0 left-0 bottom-0 flex flex-col justify-center items-center w-96 bg-white h-screen px-1  "
     >
-      <motion.div
-        variants={varients}
-        className="absolute top-0 left-0 bottom-0 flex flex-col justify-center items-center w-[50vw] bg-white h-screen px-1  "
+      <a
+        href="#"
+        className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
+        onClick={handleClick}
       >
-        <a
-          href="#"
-          className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
-        >
-          Home
-        </a>
-        <a
-          href="#skill"
-          className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
-        >
-          Skills
-        </a>
-        <a
-          href="#project"
-          className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
-        >
-          Projects
-        </a>
-        <a
-          href="#about"
-          className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
-        >
-          About
-        </a>
-        <a
-          href="#contact"
-          className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
-        >
-          Contact
-        </a>
-      </motion.div>
+        Home
+      </a>
+      <a
+        href="#skill"
+        className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
+      >
+        Skills
+      </a>
+      <a
+        href="#project"
+        className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
+      >
+        Projects
+      </a>
+      <a
+        href="#about"
+        className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
+      >
+        About
+      </a>
+      <a
+        href="#contact"
+        className="w-full font-lora font-semibold text-lg border border-gray-300 py-2 text-center"
+      >
+        Contact
+      </a>
     </motion.div>
   );
 };
