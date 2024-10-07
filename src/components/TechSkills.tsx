d78ffe2b82bfebef61aa68dbab4b4499467f105c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-07 20:46:10 +0630

    update tech skills card

--- src/components/TechSkills.tsx
+++ src/components/TechSkills.tsx
@@ -15,56 +15,47 @@ interface SkillsProp {
 
 const TechSkills: FC<SkillsProp> = ({ title, skill }) => {
   return (
-    <section className="w-full">
-      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-lora font-semibold mb-4">
+    <section className="w-full space-y-4 ">
+      <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-lora font-semibold ">
         {title}
       </h1>
-      <section className="flex flex-wrap items-center justify-center gap-6  ">
+      <section className="flex flex-wrap items-center justify-center md:justify-start gap-4   ">
         {skill.map((skill, i) => (
           <div
             key={i}
-            className={`group relative flex flex-col items-center justify-center w-32 h-36 rounded-md shadow-md gap-2 border ${
-              skill.count > 79
-                ? "border-emerald-400"
-                : skill.count > 60
-                ? "border-cyan-400"
-                : "border-rose-400"
-            }  `}
+            className="group relative flex  items-center  w-40  rounded-md shadow-md bg-purple-100 gap-2 border-2 border-cyan-300
+              "
           >
-            <div className="size-14 flex justify-center items-center overflow-hidden bg-white rounded-full b ">
+            <div className="relative z-30 size-12 flex justify-center items-center overflow-hidden rounded-md bg-black  m-2">
               <Image
                 src={skill.icon}
                 width={40}
                 height={40}
                 alt={skill.name}
-                className="group-hover:hidden"
+                className="group-hover:hidden "
               />
               <div
-                className={`hidden group-hover:block  font-poppin ${
-                  skill.count > 79
-                    ? "text-emerald-400"
-                    : skill.count > 60
-                    ? "text-cyan-400"
-                    : "text-rose-400"
-                } `}
+                className={`hidden group-hover:block  font-poppin text-white`}
               >
                 {skill.count}%
               </div>
             </div>
 
-            <p className="font-poppin font-medium text-sm text-white">
+            <p className="relative z-30 font-poppin font-semibold text-sm text-black mr-2">
               {skill.name}
             </p>
 
             <div
-              className={`absolute bottom-0 -z-10 w-full  h-[65%] ${
+              className={`absolute bottom-0 z-20 w-0  group-hover:w-full h-full transition-all duration-500 ease-out ${
                 skill.count > 79
                   ? "bg-emerald-400"
                   : skill.count > 60
                   ? "bg-cyan-400"
                   : "bg-rose-400"
               } `}
-            ></div>
+            >
+              {/* <div className=" relative bottom-0 w-full h-0  group-hover:h-full bg-red-900 transition-all duration-500 ease-out  "></div> */}
+            </div>
           </div>
         ))}
       </section>
