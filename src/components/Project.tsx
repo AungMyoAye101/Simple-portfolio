a9b8b6b80a8b1744b19a6d3d1611ee5f13c926ce
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-08 19:55:42 +0630

    add animation to project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,46 +1,71 @@
+"use client";
 import React from "react";
 import { projects } from "@/data";
 import Image from "next/image";
 import { FaLocationArrow } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 const Project = () => {
   return (
     <section className="section-container" id="project">
       <h1 className="heading">My Recent Projects</h1>
-      <main className="w-full max-w-5xl flex flex-col gap-8 md:gap-12  ">
+      <main className="w-full max-w-5xl flex flex-col gap-12  ">
         {projects.map((project, i) => (
-          <div
+          <motion.div
+            initial={{
+              opacity: 0,
+              scale: 0.8,
+            }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ ease: "easeOut", duration: 1 }}
+            exit={{ opacity: 0 }}
             key={i}
-            className="group relative flex flex-col md:flex-row  justify-between  p-0 md:p-4 gap-4 rounded-md shadow-md overflow-hidden border md:border-cyan-500 "
+            className="group relative flex flex-col md:flex-row  justify-between  p-0 md:p-4 gap-4 rounded-md shadow-md overflow-hidden md:border border-cyan-500 "
           >
-            <div className="relative w-full  md:w-[45%] bg-gray-600 h-48 sm:h-80  md:h-64 md:rounded-md overflow-hidden">
-              <Image src={project.image} fill alt="project name" className="" />
-            </div>
-            <div className="flex flex-col gap-2 justify-between  p-2 w-full md:w-[50%]">
-              <div className="space-y-2">
-                <h1 className="text-xl md:text-2xl font-lora font-semibold">
-                  {project.title}
-                </h1>
-                <p className="text-sm md:text-base hidden md:block font-poppin  font-medium line-clamp-2 md:line-clamp-none">
-                  {project.description}
-                </p>
-                <div className="hidden md:flex flex-wrap gap-2 ">
-                  {project.iconList.map((icon, idx) => (
-                    <div
-                      key={idx}
-                      className="flex  gap-1 items-center p-1 shadow-md rounded-md border border-gray-200"
-                    >
-                      <Image
-                        src={icon.icon}
-                        width={20}
-                        height={20}
-                        alt="icons"
-                      />
-                      <span>{icon.name}</span>
-                    </div>
-                  ))}
-                </div>
+            <motion.div
+              initial={{ opacity: 0.2, scale: 0.5 }}
+              whileInView={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 1.5,
+                ease: "easeIn",
+              }}
+              className="relative w-full  md:w-[45%] h-[30vh] md:h-[45vh] md:rounded-md overflow-hidden bg-gray-600"
+            >
+              <Image
+                src={project.image}
+                fill
+                alt="project name"
+                className="object-cover"
+              />
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0.8, y: 20 }}
+              whileInView={{ opacity: 1, y: 0 }}
+              transition={{
+                ease: "easeIn",
+                staggerChildren: 1,
+                delay: 1,
+              }}
+              exit={{ opacity: 0 }}
+              className="flex flex-col gap-2 justify-between   p-2 w-full md:w-[50%]"
+            >
+              <h1 className="text-xl md:text-2xl font-lora font-semibold">
+                {project.title}
+              </h1>
+              <p className="text-sm md:text-base hidden md:block font-poppin  font-medium line-clamp-2 md:line-clamp-none">
+                {project.description}
+              </p>
+              <div className="hidden md:flex flex-wrap gap-2 ">
+                {project.iconList.map((icon, idx) => (
+                  <div
+                    key={idx}
+                    className="flex  gap-1 items-center p-1 shadow-md rounded-md border border-cyan-500"
+                  >
+                    <Image src={icon.icon} width={20} height={20} alt="icons" />
+                    <span>{icon.name}</span>
+                  </div>
+                ))}
               </div>
               <div className=" hidden md:flex justify-between items-center pb-3">
                 <a
@@ -58,12 +83,14 @@ const Project = () => {
                   <FaLocationArrow />
                 </a>
               </div>
-            </div>
-            {/* 
-            <div className="hidden  group-hover:flex md:hidden flex-col gap-2 p-4  justify-center items-center absolute w-full h-full bg-cyan-100 transition-all duration-500 ease-out ">
-              <p>{project.description}</p>
+            </motion.div>
 
-              <div className="w-full flex justify-between items-center ">
+            {/* <div className="opacity-0  group-hover:opacity-100  flex flex-col gap-2 p-4 py-8  justify-between items-center absolute w-full h-full bg-cyan-300 transition-all duration-500 ease-out ">
+              <p className="text-neutral-800 font-lora ">
+                {project.description}
+              </p>
+
+              <div className="w-full flex justify-between items-center text-neutral-800 ">
                 <a
                   href="#"
                   className="text-lora font-medium text-sm md:text-base flex items-center gap-1 "
@@ -80,7 +107,7 @@ const Project = () => {
                 </a>
               </div>
             </div> */}
-          </div>
+          </motion.div>
         ))}
       </main>
 
