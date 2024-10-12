456ae140ef2a860598069442f0cb35d733354672
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-12 13:52:21 +0630

    add project detail page

--- src/app/project/[id]/page.tsx
+++ src/app/project/[id]/page.tsx
@@ -1,9 +1,25 @@
+import { projects } from "@/data";
+import Image from "next/image";
 import React from "react";
 
 const page = ({ params }: { params: { id: number } }) => {
+  const project = projects[params.id];
   return (
     <section className="section-container">
-      <h1>hello{params.id}</h1>
+      <div className="flex flex-col md:felx-row gap-10 justify-center items-center ">
+        <div className="w-[45%]">
+          <Image
+            src={`/${project.image}`}
+            width={400}
+            height={400}
+            alt="nama"
+          />
+        </div>
+        <div className="w-[50%]">
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+        </div>
+      </div>
     </section>
   );
 };
