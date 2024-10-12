e068e1413e8e6e82126878694705bd314de4689c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-12 21:27:02 +0630

    add loading ui

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const loading = () => {
+  return (
+    <div className="flex justify-center items-center min-h-screen ">
+      loading..
+    </div>
+  );
+};
+
+export default loading;
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,57 +5,49 @@ import { FaFacebookF } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <section className="flex flex-col items-center gap-4 ">
-      <a
-        href="#"
-        className="bg-cyan-400 p-3 rounded-full animate-bounce   shadow-md  "
-      >
-        <FaArrowUp className="text-lg text-white " />
-      </a>
-      <footer className="w-full py-12  px-10 rounded-t-md flex flex-wrap gap-6 items-center justify-center md:justify-between bg-neutral-900">
-        <div className="text-xl font-poppin font-semibold ">
-          Aung Myo Aye - Portfolio
-        </div>
+    <footer className="w-full py-12  px-10 rounded-t-md flex flex-wrap gap-6 items-center justify-center md:justify-between bg-neutral-900">
+      <div className="text-xl font-poppin font-semibold ">
+        Aung Myo Aye - Portfolio
+      </div>
 
-        <div className="flex gap-4">
-          <a
-            href="https://github.com/AungMyoAye101"
-            target="_blank"
-            className="p-3 border border-cyan-400  rounded-full shadow-md"
-          >
-            <FaGithub className="text-xl " />
-          </a>
-          <a
-            href="https://www.facebook.com/profile.php?id=61556195843942"
-            target="_blank"
-            className="p-3 border border-cyan-400  rounded-full shadow-md"
-          >
-            <FaFacebookF className="text-xl  " />
-          </a>
-          <a
-            href=""
-            target="_blank"
-            className="p-3 border border-cyan-400  rounded-full shadow-md"
-          >
-            <FaLinkedin className="text-xl  " />
-          </a>
-          <a
-            href="https://www.instagram.com/aungmyoaye404/"
-            target="_blank"
-            className="p-3 border border-cyan-400  rounded-full shadow-md"
-          >
-            <FaInstagram className="text-xl  " />
-          </a>
-        </div>
+      <div className="flex gap-4">
         <a
-          href="mailto:aungmyoaye101@gmail.com"
-          className="flex gap-1 items-center font-medium font-poppin text-white px-4 py-2 rounded-lg shadow-md  bg-cyan-500"
+          href="https://github.com/AungMyoAye101"
+          target="_blank"
+          className="p-3 border border-cyan-400  rounded-full shadow-md"
         >
-          <span> Contact Me</span>
-          <FaLocationArrow />
+          <FaGithub className="text-xl " />
         </a>
-      </footer>
-    </section>
+        <a
+          href="https://www.facebook.com/profile.php?id=61556195843942"
+          target="_blank"
+          className="p-3 border border-cyan-400  rounded-full shadow-md"
+        >
+          <FaFacebookF className="text-xl  " />
+        </a>
+        <a
+          href=""
+          target="_blank"
+          className="p-3 border border-cyan-400  rounded-full shadow-md"
+        >
+          <FaLinkedin className="text-xl  " />
+        </a>
+        <a
+          href="https://www.instagram.com/aungmyoaye404/"
+          target="_blank"
+          className="p-3 border border-cyan-400  rounded-full shadow-md"
+        >
+          <FaInstagram className="text-xl  " />
+        </a>
+      </div>
+      <a
+        href="mailto:aungmyoaye101@gmail.com"
+        className="flex gap-1 items-center font-medium font-poppin text-white px-4 py-2 rounded-lg shadow-md  bg-cyan-500"
+      >
+        <span> Contact Me</span>
+        <FaLocationArrow />
+      </a>
+    </footer>
   );
 };
 
