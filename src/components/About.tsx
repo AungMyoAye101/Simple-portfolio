69b6ac8007f5398c3861e26324e878c6eccd536c
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-14 07:51:06 +0630

    update about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,30 +6,53 @@ import { animateText } from "./Skills";
 import TextReveal from "./ui/TextReveal";
 import Button from "./ui/Button";
 
+const imageVarient = {
+  initial: {
+    opacity: 0,
+    scale: 0,
+  },
+  inView: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      ease: "easeIn",
+      duration: 0.7,
+    },
+  },
+  animate: {
+    backgroundPosition: ["0% 50%", "100% 50%"],
+    transition: {
+      ease: "linear",
+      duration: 5,
+      repeat: Infinity,
+    },
+  },
+};
+
 const About = () => {
   return (
     <section className="min-h-screen py-20 px-4 md:px-12 " id="about">
       <h1 className="heading">About Me</h1>
       <div className="flex flex-col md:flex-row justify-between  gap-8 py-8 ">
         <motion.div
-          initial={{
-            opacity: 0,
-            scale: 0.5,
+          variants={imageVarient}
+          initial="initial"
+          whileInView="inView"
+          animate="animate"
+          className=" relative w-[90%] md:w-[40%] h-[40vh] md:h-[65vh] border-2   mx-auto  bg-transparent   border-transparent bg-clip-border rounded-md bg-gradient-to-tr from-rose-500 via-amber-500 to-blue-600 "
+          style={{
+            backgroundImage: "linear-gradient(to right,red,orange ,violet)",
+            backgroundSize: "200% 200%",
           }}
-          whileInView={{ opacity: 1, scale: 1 }}
-          transition={{
-            ease: "easeIn",
-            duration: 0.7,
-          }}
-          className=" relative w-[90%] md:w-[40%] h-[40vh] md:h-[65vh] border-2 border-cyan-400 rounded z-20 mx-auto"
         >
+          <div className="abouslute  w-full h-full m-4 rounded-md z-40 border-2 border-cyan-300"></div>
           <Image
             src={"/elizabeth.png"}
             fill
             alt="profile"
-            className="object-cover "
+            className="object-cover relative z-20"
           />
-          <div className="abouslute  w-full h-full m-4 rounded border-2 border-emerald-400 z-10"></div>
+          <span className="absolute inset-0 bg-black z-0 rounded-md"></span>
         </motion.div>
         <motion.div
           variants={animateText}
