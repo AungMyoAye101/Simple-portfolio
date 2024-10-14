3680c38b21aa3ca8d3729daf96a360a36a0a4dbe
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-14 14:16:18 +0630

    add form ui

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,10 +1,9 @@
 "use client";
 
 import React, { useRef } from "react";
-import emailjs from "@emailjs/browser";
+
 import { motion, useInView } from "framer-motion";
-import { useFormStatus } from "react-dom";
-import SubmitButton from "./ui/SubmitButton";
+import Form from "./ui/form";
 
 const varients = {
   initial: {
@@ -22,29 +21,8 @@ const varients = {
 };
 
 const Contact = () => {
-  const { pending } = useFormStatus();
-  const form = useRef<HTMLFormElement>(null);
   const ref = useRef(null);
   const inView = useInView(ref, { margin: "-100px" });
-
-  const sendEmail = () => {
-    if (!form.current) return;
-    emailjs
-      .sendForm(
-        "service_jfthy5f",
-        "template_jn7xbx8",
-        form.current,
-        "uE4oObU-ushf6zRRS"
-      )
-      .then(
-        () => {
-          console.log("SUCCESS!");
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
-  };
 
   return (
     <section className="section-container" id="contact">
@@ -87,54 +65,7 @@ const Contact = () => {
               ></motion.path>
             </svg>
           </motion.div>
-          <motion.form
-            initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{
-              delay: 4,
-              duration: 1,
-            }}
-            ref={form}
-            action={sendEmail}
-            className=" w-full md:w-[60%] grid gap-4 "
-          >
-            <div className="flex flex-col gap-1 ">
-              <input
-                type="text"
-                id="Name"
-                name="name"
-                disabled={pending}
-                placeholder="Enter your  Name"
-                className="input-style"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1 ">
-              <input
-                type="email"
-                id="email"
-                name="email"
-                placeholder="Enter your email address"
-                className="input-style"
-              />
-            </div>
-
-            <div className="flex flex-col gap-1  ">
-              <textarea
-                id="text"
-                name="message"
-                placeholder="I value your opinion"
-                className="input-style h-28 "
-              />
-            </div>
-            {/* <button
-              type="submit"
-              className="px-6 py-2 rounded-lg shadow-md border-2 border-cyan-400  font-lora text-lg hover:scale-105 hover:shadow-lg transition-all ease-out duration-200 "
-            >
-              {pending ? "Submiting..." : "Submit"}
-            </button> */}
-            <SubmitButton />
-          </motion.form>
+          <Form />
         </motion.div>
         <motion.p
           variants={varients}
--- src/components/ui/form.tsx
+++ src/components/ui/form.tsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useRef } from "react";
+import SubmitButton from "./SubmitButton";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { useFormStatus } from "react-dom";
 
 const Form = () => {
-  return <div>form</div>;
+  const { pending } = useFormStatus();
+  const form = useRef<HTMLFormElement>(null);
+  const sendEmail = () => {
+    if (!form.current) return;
+    emailjs
+      .sendForm(
+        "service_jfthy5f",
+        "template_jn7xbx8",
+        form.current,
+        "uE4oObU-ushf6zRRS"
+      )
+      .then(
+        () => {
+          console.log("SUCCESS!");
+        },
+        (error) => {
+          console.log("FAILED...", error.text);
+        }
+      );
+  };
+  return (
+    <motion.form
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{
+        delay: 4,
+        duration: 1,
+      }}
+      ref={form}
+      action={sendEmail}
+      className=" w-full md:w-[60%] grid gap-4 "
+    >
+      <div className="flex flex-col gap-1 ">
+        <input
+          type="text"
+          id="Name"
+          name="name"
+          disabled={pending}
+          placeholder="Enter your  Name"
+          className="input-style"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1 ">
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="Enter your email address"
+          className="input-style"
+        />
+      </div>
+
+      <div className="flex flex-col gap-1  ">
+        <textarea
+          id="text"
+          name="message"
+          placeholder="I value your opinion"
+          className="input-style h-28 "
+        />
+      </div>
+      {/* <button
+    type="submit"
+    className="px-6 py-2 rounded-lg shadow-md border-2 border-cyan-400  font-lora text-lg hover:scale-105 hover:shadow-lg transition-all ease-out duration-200 "
+  >
+    {pending ? "Submiting..." : "Submit"}
+  </button> */}
+      <SubmitButton />
+    </motion.form>
+  );
 };
 
 export default Form;
