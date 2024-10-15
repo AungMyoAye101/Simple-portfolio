3cac603f38892c9e25ebcd9fd30ee01488311d7f
AungMyoAye101 <aungmyoaye101@gmail.com>
2024-10-15 23:07:59 +0630

    update btn

--- src/components/ui/BgGradientBtn.tsx
+++ src/components/ui/BgGradientBtn.tsx
@@ -5,14 +5,29 @@ import { FaArrowAltCircleRight, FaLink } from "react-icons/fa";
 
 const BggradientBtn = ({ text }: { text: string }) => {
   return (
-    <a
+    <motion.a
+      animate={{
+        backgroundPosition: ["0% 50%", "100% 50%"],
+      }}
+      transition={{
+        duration: 5,
+        repeat: Infinity,
+        repeatType: "mirror",
+        ease: "linear",
+      }}
+      style={{
+        backgroundPosition: "200% 200%",
+      }}
       href="#"
-      className="flex items-center  gap-1 text-lg  font-semibold font-poppin bg-gradient-to-r hover:bg-gradient-to-l from-orange-400 via-purple-400 to-cyan-400 rounded-full px-4 py-2"
+      className="flex items-center  gap-1 text-lg  font-semibold font-poppin bg-gradient-to-r  from-cyan-400 via-cyan-600 to-cyan-900 rounded-full px-4 py-2"
     >
-      <span>{text}</span>
-
-      <FaArrowAltCircleRight />
-    </a>
+      <motion.span initial={{ x: 0 }} whileHover={{ x: -20 }}>
+        {text}
+      </motion.span>
+      <motion.span initial={{ rotate: 0 }} whileHover={{ rotate: "180deg" }}>
+        <FaArrowAltCircleRight />
+      </motion.span>
+    </motion.a>
   );
 };
 export default BggradientBtn;
